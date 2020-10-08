65505dc7197d342fe05b4ab7d3d8f44ce23594dc
Theerasak Tubrit <jigkoh3@Macintoshs-MacBook-Pro.local>
2020-10-08 20:26:57 +0700

    ok2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'reg-sin';
+
+  public getCapture(event) {
+    console.log(event);
+  }
 }
